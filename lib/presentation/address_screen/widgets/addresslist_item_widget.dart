39c37915c1b2009e64709f3ed80ee8010c77d133
saurabhkus7415 <saurabhkus7415@gmail.com>
2024-01-09 18:18:32 +0530

    added create update and delete mathod with api

--- lib/presentation/address_screen/widgets/addresslist_item_widget.dart
+++ lib/presentation/address_screen/widgets/addresslist_item_widget.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shoper_flutter/core/app_export.dart';
+import 'package:shoper_flutter/core/service/api_service.dart';
+import 'package:shoper_flutter/presentation/add_address_screen/add_address_screen.dart';
+import 'package:shoper_flutter/presentation/address_screen/address_screen.dart';
 
 class AddresslistItemWidget extends StatelessWidget {
   final dynamic addressData;
+  final ApiService _apiService = ApiService();
 
   AddresslistItemWidget({Key? key, required this.addressData})
       : super(key: key);
@@ -55,20 +59,92 @@ class AddresslistItemWidget extends StatelessWidget {
           SizedBox(height: 19.v),
           Row(
             children: [
-              Text(
-                "lbl_edit".tr,
-                style: CustomTextStyles.titleSmallPrimary,
+              GestureDetector(
+                onTap: () {
+                  onEditPressed(context, addressData);
+                },
+                child: Text(
+                  "lbl_edit".tr,
+                  style: CustomTextStyles.titleSmallPrimary,
+                ),
               ),
               SizedBox(width: 32.h),
-              Text(
-                "lbl_delete".tr,
-                style: CustomTextStyles.titleSmallPink300,
+              GestureDetector(
+                onTap: () {
+                  onDeletePressed(context, addressData['_id']);
+                },
+                child: Text(
+                  "lbl_delete".tr,
+                  style: CustomTextStyles.titleSmallPink300,
+                ),
               ),
             ],
-          ),
-          SizedBox(height: 3.v),
+          )
         ],
       ),
+    );
+  }
+
+  void onEditPressed(BuildContext context, Map<String, dynamic> addressData) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AddAddressScreen(addressData: addressData),
+      ),
+    );
+  }
+
+  void onDeletePressed(BuildContext context, String address) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirmation'),
+          content: Text('Are you sure you want to delete this address?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context).pop(); // Close the dialog
+
+                try {
+                  final response = await _apiService
+                      .deteleData('api/v1/address/delete/${address}');
+
+                  if (response.statusCode == 202) {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AddressScreen()));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Failed. Please check.'),
+                        duration: Duration(seconds: 3),
+                      ),
+                    );
+                  }
+                } catch (error) {
+                  // Display an error message
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content:
+                          Text('An error occurred. Please try again later.'),
+                      duration: Duration(seconds: 3),
+                    ),
+                  );
+                }
+              },
+              child: Text('Delete'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
