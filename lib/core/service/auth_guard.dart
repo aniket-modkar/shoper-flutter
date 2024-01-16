2d379de2971836f1463af1efd6b64dd5374ffa1e
saurabhkus7415 <saurabhkus7415@gmail.com>
2024-01-16 17:14:58 +0530

    added changes with order listing fixed

--- lib/core/service/auth_guard.dart
+++ lib/core/service/auth_guard.dart
@@ -16,6 +16,12 @@ class AuthGuard extends RouteObserver<PageRoute<dynamic>> {
       Navigator.of(route.navigator!.context).pushReplacementNamed(
         AppRoutes.loginScreen,
       );
+    } else if (token.isNotEmpty &&
+        route.settings.name == AppRoutes.loginScreen) {
+      // If the token is valid and the current route is the login screen, navigate to the dashboardContainerScreen
+      Navigator.of(route.navigator!.context).pushReplacementNamed(
+        AppRoutes.dashboardContainerScreen,
+      );
     }
   }
 
